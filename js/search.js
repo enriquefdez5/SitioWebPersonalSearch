1ec0b0a309c2578e72a327fc6d3eb3856c005e15
Enrique Fernández Manzano <enriquefernandezmanzano@MacBook-Air-de-Enrique.local>
2021-12-13 23:45:22 +0100

    search engine

--- js/search.js
+++ js/search.js
@@ -1,31 +1,52 @@
 function search() {
     let input = $("#searchInput").val()
-    let map = {}
-    $("#search").load("index.html #main", function (data){
-        recursiveEach($(data))
+    let enlaces = []
+    $("#search").load("index.html #main", function (data) {
+        recursiveEach($(data), enlaces, input)
+        for (enlace of enlaces)
+        console.log("enlaces")
+        console.log(enlaces)
     })
 
-    function recursiveEach(element){
-        element.children().each(function () {
-            var currentElement = $(this);
-            for (let elem of currentElement) {
-                if (elem != undefined && elem.innerText.toLowerCase().includes(input.toLowerCase()) ) {
-                    console.log("elem")
-                    console.log(elem)
-                    console.log("elem.innerText")
-                    console.log(elem.innerText)
+}
+
+function recursiveEach(element, enlaces, input){
+    element.children().each(function () {
+        var currentElement = $(this);
+        for (let elem of currentElement) {
+            if (elem != undefined && elem.innerText.toLowerCase().includes(input.toLowerCase()) ) {
+                console.log("elem")
+                console.log(elem)
+                console.log("elem.innerText")
+                console.log(elem.innerText)
+                if (elem.closest("section") != undefined && elem.closest("section") != null) {
                     console.log("closest")
                     console.log(elem.closest("section"))
-                    // $(elem).attr("refe")
-                    // objeto.add({
-                    //     "contenido": elem.innerText.length > 20 ? elem.innerText.substr(0, 20) : elem.innerText,
-                    //     "referencia": input + i.toString
-                    // })
+                    enlaces.push({
+                        contenido: elem.innerText,
+                        enlace: "index.html#"+elem.closest("section").getAttribute("id")
+                    })
+                }
+                else if (elem.closest("footer") != undefined && elem.closest("footer") != null) {
+                    console.log("closest")
+                    console.log(elem.closest("footer"))
+                    enlaces.push({
+                        contenido: elem.innerText,
+                        enlace: "index.html#"+elem.closest("footer").getAttribute("id")
+                    })
+                }
+                else {
+                    console.log("closest")
+                    console.log(elem.closest("main"))
+                    enlaces.push({
+                        contenido: elem.innerText,
+                        enlace: "index.html#main"
+                    })
                 }
             }
-            // Loop her children
-            recursiveEach(currentElement);
-        });
-    }
+        }
+        // Loop her children
+        recursiveEach(currentElement, enlaces, input);
+    });
 }
 
