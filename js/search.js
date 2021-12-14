6e9280085e95112e73bee18b11d2832bc2fdc8d2
Enrique Fernández Manzano <uo257742@uniovi.es>
2021-12-14 01:10:32 +0100

    first prototype

--- js/search.js
+++ js/search.js
@@ -1,43 +1,46 @@
-function search() {
-    let input = $("#searchInput").val()
-    let enlaces = []
+let enlaces = []
+window.addEventListener("load", () => {
+    const urlParams = new URLSearchParams(window.location.search)
+    let input = urlParams.get("input")
     $("#search").load("index.html #main", function (data) {
         recursiveEach($(data), enlaces, input)
-        for (enlace of enlaces)
-        console.log("enlaces")
-        console.log(enlaces)
+        for (enlace of enlaces) {
+            $("#main2").append("<div class='container'><a href='" + enlace.enlace + "'>" + enlace.enlace + "</a><p>" + enlace.contenido + "</p></div>")
+        }
     })
+})
+function search() {
+    let input = $("#searchInput").val()
+    window.location.href = "busquedas.html?input="+input
 
+    // let input = $("#searchInput").val()
+    // $("#search").load("index.html #main", function (data) {
+    //     recursiveEach($(data), enlaces, input)
+    // })
+    // window.location.href = "busquedas.html"
 }
 
 function recursiveEach(element, enlaces, input){
     element.children().each(function () {
         var currentElement = $(this);
         for (let elem of currentElement) {
-            if (elem != undefined && elem.innerText.toLowerCase().includes(input.toLowerCase()) ) {
-                console.log("elem")
-                console.log(elem)
-                console.log("elem.innerText")
-                console.log(elem.innerText)
-                if (elem.closest("section") != undefined && elem.closest("section") != null) {
-                    console.log("closest")
-                    console.log(elem.closest("section"))
+            console.log(elem.innerText.toLowerCase())
+            console.log(input.toLowerCase())
+            console.log(elem.innerText.toLowerCase().includes(input.toLowerCase()))
+            if (elem.innerText.toLowerCase().includes(input.toLowerCase()) ) {
+                if (elem.closest("section") !== undefined && elem.closest("section") != null) {
                     enlaces.push({
                         contenido: elem.innerText,
                         enlace: "index.html#"+elem.closest("section").getAttribute("id")
                     })
                 }
-                else if (elem.closest("footer") != undefined && elem.closest("footer") != null) {
-                    console.log("closest")
-                    console.log(elem.closest("footer"))
+                else if (elem.closest("footer") !== undefined && elem.closest("footer") != null) {
                     enlaces.push({
                         contenido: elem.innerText,
                         enlace: "index.html#"+elem.closest("footer").getAttribute("id")
                     })
                 }
                 else {
-                    console.log("closest")
-                    console.log(elem.closest("main"))
                     enlaces.push({
                         contenido: elem.innerText,
                         enlace: "index.html#main"
