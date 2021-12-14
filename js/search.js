09b523019c6fecb626f5979250cdf70d764b5651
Enrique Fernández Manzano <uo257742@uniovi.es>
2021-12-14 23:22:11 +0100

    buscador terminado

--- js/search.js
+++ js/search.js
@@ -1,24 +1,29 @@
 function mostrarEnlace(enlace) {
-    console.log("enlace")
-    console.log(enlace)
-    console.log(enlace.includes("index"))
-    let retstr = ""
-    switch (enlace){
-        case enlace.includes("index"): console.log("ey"); retstr = "Perfil"; break;
-        case enlace.includes("aficiones"): retstr = "Aficiones"; break;
-        case enlace.includes("musica"): retstr = "Música"; break;
-        case enlace.includes("pueblo"): retstr = "Pueblo"; break;
+    switch (true){
+        case enlace.includes("index"):
+            return "Perfil";
+            break;
+        case enlace.includes("aficiones"):
+            return "Aficiones";
+            break;
+        case enlace.includes("musica"):
+            return "Música";
+            break;
+        case enlace.includes("pueblo"):
+            return "Pueblo";
+            break;
+        default:
+            return "holita";
+            break;
     }
-    return retstr
 }
 
 let enlaces = []
-let str = "<h2>Resultados de la búsqueda</h2><div id='resultados' class='container'>"
+let str = "<div class='container'> <h2>Resultados de la búsqueda</h2><p>Se han encontrado "
 
 window.addEventListener("load", () => {
     const urlParams = new URLSearchParams(window.location.search)
     let input = urlParams.get("input")
-    // let str = "<h2>Resultados de la búsqueda</h2><div id='resultados' class='container'>"
     $("#search").load("index.html #main", function (data) {
         recursiveEach($(data), enlaces, input, "index.html")
         $("#search").load("aficiones.html #main", function (data2) {
@@ -27,10 +32,11 @@ window.addEventListener("load", () => {
                 recursiveEach($(data3), enlaces, input, "musica.html")
                 $("#search").load("pueblo.html #main", function (data4) {
                     recursiveEach($(data4), enlaces, input, "pueblo.html")
+                    str = str.concat(enlaces.length + " resultados</p><div id='resultados' class='container'>")
                     for (enlace of enlaces) {
-                        str = str.concat("<div class='container'><a href='" + enlace.enlace + "'>" + mostrarEnlace(enlace.enlace) + "</a><p>" + enlace.contenido + "</p></div>")
+                        str = str.concat("<div class='container searchresult'><a class='grande' href='" + enlace.enlace + "'>" + mostrarEnlace(enlace.enlace) + "</a><p>" + enlace.contenido + "</p></div>")
                     }
-                    str.concat("</div)")
+                    str.concat("</div></div>")
                     $("#main2").append(str.toString())
                 })
             })
@@ -46,24 +52,26 @@ function recursiveEach(element, enlaces, input, link){
     element.children().each(function () {
         var currentElement = $(this);
         for (let elem of currentElement) {
-            if (enlaces.filter(e => e.contenido === elem.innerText).length > 0) {
-                console.log("repe!")
+            if (enlaces.filter(e => {
+                return e.contenido.split(/\s+/).join(' ').substring(0, 40).trim()
+                    .includes(elem.innerText.split(/\s+/).join(' ').substring(0, 40).trim())}).length > 0) {
+
             }
             else {
                 if (elem.innerText.toLowerCase().includes(input.toLowerCase())) {
                     if (elem.closest("section") !== undefined && elem.closest("section") != null) {
                         enlaces.push({
-                            contenido: elem.innerText.substr(0, 40),
+                            contenido: elem.innerText.split(/\s+/).join(' ').substring(0, 40) + "...",
                             enlace: link + elem.closest("section").getAttribute("id")
                         })
                     } else if (elem.closest("footer") !== undefined && elem.closest("footer") != null) {
                         enlaces.push({
-                            contenido: elem.innerText.substring(0, 40),
+                            contenido: elem.innerText.split(/\s+/).join(' ').substring(0, 40) + "...",
                             enlace: link + elem.closest("footer").getAttribute("id")
                         })
                     } else {
                         enlaces.push({
-                            contenido: elem.innerText.substring(0, 40),
+                            contenido: elem.innerText.split(/\s+/).join(' ').substring(0, 40) + "...",
                             enlace: link
                         })
                     }
